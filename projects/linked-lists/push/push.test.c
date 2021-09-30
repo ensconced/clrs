337cd5a28376c27db712ef3d815148b5ce06e6c5
Joe Barnett <joebarnett876@gmail.com>
2021-09-30 08:08:28 +0100

    wip

--- projects/linked-lists/push/push.test.c
+++ projects/linked-lists/push/push.test.c
@@ -4,10 +4,25 @@
 #include "../123/123.h"
 #include "../node.h"
 
-int main() {
+void pushToExistingList() {
   struct node* list = buildOneTwoThree();
   push(&list, 4);
   int expected[4] = {4, 1, 2, 3};
   assertListEqualToArray(list, expected, 4);
+}
+
+void buildListByPushing() {
+  struct node* listHead = NULL;
+  struct node** headRef = &listHead;
+  push(headRef, 1);
+  push(headRef, 2);
+  push(headRef, 3);
+  int expected[3] = {3, 2, 1};
+  assertListEqualToArray(*headRef, expected, 3);
+}
+
+int main() {
+  pushToExistingList();
+  buildListByPushing();
   return 0;
 }
